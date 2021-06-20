b988e3bd9ea1734c40b72d4d677160491273861b
dhruv2102 <kumardh@tcd.ie>
2021-06-20 15:12:36 +0000

    Added visualization for quicksort

--- assets/js/quick.js
+++ assets/js/quick.js
@@ -6,23 +6,49 @@ async function quicksort(elements, l, r){
         pivot = await partition(elements, l, r)
         await quicksort(elements, l, pivot-1)
         await quicksort(elements, pivot+1, r)
+    }else{
+        elements[r].style.background = 'green'
+        elements[l].style.background = 'green'
     }
 }
 
 async function partition(elements, low, high){
     let piv = elements[high].style.height;
     let i = low -1
-    await timeout(delay)
+    
+    // Selecting color of pivot element
+    elements[high].style.background = 'red'
 
     for(let j=low; j<high; j++){
+        // Select color for current element
+        elements[j].style.background = 'blue'
+        await timeout(delay)
         if(parseInt(elements[j].style.height) < parseInt(piv)){
             i++;
             swap(elements[i], elements[j]);
+            
+            elements[i].style.background = 'orange'
+
+            if(i !== j){
+                elements[j].style.background = 'orange'
+            }
             await timeout(delay)
+        }
+        else{
+             elements[j].style.background = 'pink'
         }
     }
     await timeout(delay)
     swap(elements[i+1], elements[high]);
+    elements[high].style.background = 'pink'
+    elements[i+1].style.background = 'green'
+
+    // Turning color back to black for unsorted
+    for(let c = 0; c<elements.length; c++){
+        if(elements[c].style.background !== 'green'){
+            elements[c].style.background = 'black'
+        }
+    }
     return i+1
 
 }
